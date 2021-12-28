50ea9a54833d86861fdae27fb189cbb301e895e6
kashyapSheladiya <kashyap27041993@gmail.com>
2021-12-28 18:10:20 +0100

    index action article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,11 @@
 class ArticlesController < ApplicationController
+  def index
+    articles = Article.all
+    render json: serializer.new(articles), status: :ok
+  end
+
+  private
+  def serializer
+    ArticleSerializer
+  end
 end
