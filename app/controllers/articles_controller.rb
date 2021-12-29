afd3847164bc5209513449fc022e8386348364c8
kashyapSheladiya <kashyap27041993@gmail.com>
2021-12-29 14:54:35 +0100

    show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
   include Paginable
-  
+
   def index
     articles = Article.recent
     paginated = paginate(articles)
     render_collection(paginated)
+  end
+
+  def show
+    article = Article.find params[:id]
+    render json: serializer.new(article)
   end
 
   private
